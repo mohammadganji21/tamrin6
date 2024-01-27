6f0c570e6624e192bb02de2af5b4da975cd36f24
mohammadganji21 <157909843+mohammadganji21@users.noreply.github.com>
2024-01-27 10:26:53 -0800

    Update 22.py

--- tamrin2/22.py
+++ tamrin2/22.py
@@ -4,37 +4,33 @@ def process_input():
         user_input = input()
         if user_input == "-1 -1":
             break
-        else:
-            n, b = map(int, user_input.split())
-            data.append((n, b))
+        n, b = map(int, user_input.split())
+        data.append((n, b))
     return data
 
 def find_sum_of_divisors(number):
-    divisor_sum = 0
-    for i in range(1, number + 1):
-        if number % i == 0:
-            divisor_sum += i
-    return divisor_sum
+    return sum(i for i in range(1, number + 1) if number % i == 0)
 
 def convert_to_custom_base(number, base):
-    if base < 2 or base > 9:
-        return "0"
-    result = ""
-    while number > 0:
-        result = str(number % base) + result
-        number //= base
-    return int(result) if result else 0
+    if 2 <= base <= 9:
+        result = ""
+        while number > 0:
+            result = str(number % base) + result
+            number //= base
+        return int(result) if result else 0
+    return 0
 
 def main():
     data = process_input()
     total_result = 0
     invalid_base = False
+
     for n, b in data:
         div_sum = find_sum_of_divisors(n)
         base_result = convert_to_custom_base(div_sum, b)
 
-        if base_result != "0":
-            total_result += int(base_result)
+        if base_result != 0:
+            total_result += base_result
         else:
             print("Invalid base!")
             invalid_base = True
