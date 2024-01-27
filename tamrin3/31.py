ffdb5d9b27692f0c771cc6ca2bedd846da0c71b5
mohammadganji21 <157909843+mohammadganji21@users.noreply.github.com>
2024-01-27 10:33:26 -0800

    Update 31.py

--- tamrin3/31.py
+++ tamrin3/31.py
@@ -1,11 +1,12 @@
-inputt_text = input()
-cleaned_text = inputt_text.strip()
-cleaned_text = ' '.join(cleaned_text.split())
+input_text = input().strip()
+cleaned_text = ' '.join(input_text.split())
 hash_position = cleaned_text.find('@')
+
 while hash_position != -1:
     at_position = cleaned_text.find('#', hash_position)
     if at_position != -1:
-        cleaned_text = cleaned_text[:at_position] + cleaned_text[at_position+1:]
+        cleaned_text = cleaned_text[:at_position] + cleaned_text[at_position + 1:]
     hash_position = cleaned_text.find('@', hash_position + 1)
+
 converted_text = cleaned_text.replace('\\n', '\n')
-print("Formatted Text: " + converted_text)
+print("Formatted Text:", converted_text)
